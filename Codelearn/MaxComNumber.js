57f2762020dab0ad36fe83a1d6d8effe5835f63c
b1809128 <nghoquochuy0902@gmail.com>
2021-12-05 15:40:22 +0700

    delete SlideImage folder

--- Codelearn/MaxComNumber.js
+++ Codelearn/MaxComNumber.js
@@ -1,42 +1,41 @@
 const checkPrimeNumber = (x) => {
-    var flag = true;
-    if (x < 2) {
-      flag = false;
-    } else if (x == 2) {
-      flag = true;
-    } else if (x % 2 == 0) {
-      flag = false;
-    } else {
-      for (var i = 3; i < x - 1; i += 2) {
-        if (x % i == 0) {
-          flag = false;
-          break;
-        }
+  var flag = true;
+  if (x < 2) {
+    flag = false;
+  } else if (x == 2) {
+    flag = true;
+  } else if (x % 2 == 0) {
+    flag = false;
+  } else {
+    for (var i = 3; i < x - 1; i += 2) {
+      if (x % i == 0) {
+        flag = false;
+        break;
       }
     }
-    return flag;
-  };
+  }
+  return flag;
+};
 
-
-const maxComNumbers = (n) =>{
-    var array = [];
-    for(let i = 2; i < n; i++) {
-        if(!checkPrimeNumber(i)){
-            array.push(i);
-        }
+const maxComNumbers = (n) => {
+  var array = [];
+  for (let i = 2; i < n; i++) {
+    if (!checkPrimeNumber(i)) {
+      array.push(i);
     }
-    console.log(array);
-    var numTest = array[0];
-    var sum = 0;
-    var result = 0;
-    for(var i = 0; i < array.length; i++) {
-       while(sum < n){
-           sum += array[i];
-           console.log(sum);
-           result++;
-       }
+  }
+  console.log(array);
+  var numTest = array[0];
+  var sum = 0;
+  var result = 0;
+  for (var i = 0; i < array.length; i++) {
+    while (sum < n) {
+      sum += array[i];
+      console.log(sum);
+      result++;
     }
-    console.log(result);
-}
+  }
+  console.log(result);
+};
 
-maxComNumbers(77)
+maxComNumbers(77);
