a4a801da2168d01e4a4e8813d0073d188e8e9011
b1809128 <nghoquochuy0902@gmail.com>
2021-12-18 11:12:30 +0700

    customize code DiagonalDifference.js by 1 for loop

--- Hackerrank/ProblemSolving/DiagonalDifference.js
+++ Hackerrank/ProblemSolving/DiagonalDifference.js
@@ -1,16 +1,13 @@
 const diagonalDifference = (matrix) => {
   var sum1 = 0;
   var sum2 = 0;
-  var j, x;
-  var count = 0;
+  var j;
+  var count = matrix.length-1;
   for (var i = 0; i < matrix.length; i++) {
     j = i;
     sum1 += matrix[i][j];
-  }
-  for (var i = matrix.length-1; i >= 0; i--) {
-    x = i;
-    sum2 += matrix[count][x];
-    count ++;
+    sum2 += matrix[i][count];
+    count--;
   }
   console.log(Math.abs(sum1 - sum2));
 };
