229663346635407b5ae15f4f2066ebe87d1491a5
b1809128 <nghoquochuy0902@gmail.com>
2021-12-22 22:26:46 +0700

    add Sets.js

---
+++ FreeCodeCamp/DataStructures/Sets.js
@@ -0,0 +1,68 @@
+const mySet = function () {
+  var collection = [];
+
+  this.has = function (element) {
+    return collection.indexOf(element) !== -1;
+  };
+
+  this.values = function () {
+    return collection;
+  };
+
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
+
+  this.remove = function (element) {
+    if (this.has(element)) {
+      index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+
+  this.size = function () {
+    return collection.length;
+  };
+
+  this.union = function () {
+    var unionSet = new Set();
+    var firstSet = this.values();
+    var secondSet = otherSet.values();
+    firstSet.forEach(function (e) {
+      unionSet.add(e);
+    });
+    return unionSet;
+  };
+
+  this.intersection = function (otherSet) {
+    var intersectionSet = new mySet();
+    var firstSet = this.values();
+    firstSet.forEach(function (e) {
+      if (otherSet.has(e)) intersectionSet.add(e);
+    });
+    return intersectionSet;
+  };
+
+  this.difference = function (otherSet) {
+    var differenceSet = new Set();
+    var firstSet = this.values();
+    firstSet.forEach(function (e) {
+      if (!otherSet.has(e)) differenceSet.add(e);
+    });
+    return differenceSet;
+  };
+
+  this.subset = function (otherSet) {
+    var firstSet = this.values();
+    return firstSet.every(function (value) {
+      return otherSet.has(value);
+    });
+  };
+};
+//15:52
