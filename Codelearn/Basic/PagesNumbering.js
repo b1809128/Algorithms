ba5896b87eff1554a559df162ea18cfa05bc1d47
b1809128 <nghoquochuy0902@gmail.com>
2021-12-31 14:41:11 +0700

    update  Codelearn

--- Codelearn/Basic/PagesNumbering.js
+++ Codelearn/Basic/PagesNumbering.js
@@ -1,9 +1,12 @@
 const pagesNumbering = (n) => {
     var k1 = 0,k2;
     const l = n.toString().length;
-    if(l > 0 && l < 10){
-        k1 = n
+    if(n > 0 && n < 10){
+        k1 = n;
+    }else if(n > 10 && n < 99){
+        
     }
+
 }
 
 pagesNumbering(11)
