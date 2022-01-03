94848b054db21290bf01127419c652049c8e42f8
b1809128 <nghoquochuy0902@gmail.com>
2022-01-03 14:49:42 +0700

    add mergesort

--- FreeCodeCamp/DataStructures/Sort.js
+++ FreeCodeCamp/DataStructures/Sort.js
@@ -21,8 +21,8 @@ class Sort {
 
   //Sap xep 2 thang ke nhau thang nao lon thi noi len
   bubbleSort = () => {
-    for (let i = 0; i < this.data.length - 1; i++) {
-      for (let j = 0; j < this.data.length - 1 - i; j++) {
+    for (let i = 0; i < this.length() - 1; i++) {
+      for (let j = 0; j < this.length() - 1 - i; j++) {
         if (this.data[j] > this.data[j + 1]) {
           this.swap(j, j + 1);
         }
@@ -30,10 +30,21 @@ class Sort {
     }
     return this.data;
   };
-
+  //Tot nhat: O(n)
   bubbleSortOrderLast = () => {
-    for (let i = 0; i < this.data.length - 1; i++) {
-      for (let j = this.data.length - 1; j > i; j--) {
+    for (let i = 0; i < this.length() - 1; i++) {
+      for (let j = this.length() - 1; j > i; j--) {
+        if (this.data[j] < this.data[j - 1]) {
+          this.swap(j, j - 1);
+        }
+      }
+    }
+    return this.data;
+  };
+  //Tot nhat: O(n)
+  insertSort = () => {
+    for (let i = 1; i < this.length(); i++) {
+      for (let j = i; j >= 0; j--) {
         if (this.data[j] < this.data[j - 1]) {
           this.swap(j, j - 1);
         }
@@ -42,11 +53,12 @@ class Sort {
     return this.data;
   };
 
+  //Tot nhat: O(n^2)
   selectionSort = () => {
     var min = 0;
-    for (let i = 0; i < this.data.length - 1; i++) {
+    for (let i = 0; i < this.length() - 1; i++) {
       min = i;
-      for (let j = i + 1; j < this.data.length; j++) {
+      for (let j = i + 1; j < this.length(); j++) {
         if (this.data[min] > this.data[j]) {
           min = j;
         }
@@ -57,6 +69,7 @@ class Sort {
     return this.data;
   };
 
+  //Tot nhat: O(nlog(n))
   quickSort = (data) => {
     if (data.length < 2) return data;
     const pivotIndex = data.length - 1;
@@ -75,8 +88,29 @@ class Sort {
     }
     return [...this.quickSort(left), pivot, ...this.quickSort(right)];
   };
+
+  merge = (left, right) => {
+    let array = [];
+    while (left.length && right.length) {
+      if(left[0] < right[0]){
+        array.push(left.shift());
+      }else{
+        array.push(right.shift());
+      }
+    }
+    return [...array,...left,...right];
+  }
+
+  mergeSort = (data)=> {
+    const p = data.length / 2;
+    if(data.length < 2) return data;
+    const left = data.splice(0,p);
+    // console.log([left,data]);
+    return this.merge(this.mergeSort(left),this.mergeSort(data) )
+  }
 }
 
 const test = [1, 2, 4, 5, 1, 3, 8, 5];
 const arr1 = new Sort(test);
-console.log(arr1.quickSort(arr1.data));
+console.log(arr1.mergeSort(arr1.data));
+// arr1.mergeSort(arr1.data);
